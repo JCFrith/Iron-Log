c63838686f1057321397fb303d990b83a02638c2
JCFrith <Chase@413Solution.com>
2026-07-15 07:09:47 -0500

    Build functional workout logging controller

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './styles.css'
 import { loadAthleteContext, saveSessions } from './supabase.js'
 import { views } from './views.js'
 import { LEAN_MASS_PROGRAM, createDraft, todayWorkoutId } from './workoutProgram.js'
-import { clearDraft, loadDraft, loadLocalSessions, saveDraft, saveLocalSessions } from './workoutStore.js'
+import { clearDraft, loadDraft, loadLocalSessions, mergeSessions, saveDraft, saveLocalSessions } from './workoutStore.js'
 
-const navigation = [['dashboard', 'Command'], ['workout', 'Workout'], ['analytics', 'Analytics'], ['body', 'Body'], ['trainer', 'AI
+const navigation = [['dashboard', 'Command'], ['workout', 'Workout'], ['analytics', 'Analytics'], ['body', 'Body'], ['trainer', 'AI Trainer

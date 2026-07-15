685b121ab4b0b42facfeca4f1d9ddc688619233e
JCFrith <Chase@413Solution.com>
2026-07-15 08:52:21 -0500

    Repair v2 controller and restore build

--- src/main.js
+++ src/main.js
@@ -4,4 +4,8 @@ import { views } from './views.js'
 import { LEAN_MASS_PROGRAM, createDraft, todayWorkoutId } from './workoutProgram.js'
 import { clearDraft, loadDraft, loadLocalSessions, mergeSessions, saveDraft, saveLocalSessions } from './workoutStore.js'
 
-const navigation = [['dashboard', 'Command'], ['workout', 'Workout'], ['analytics', 'Analytics'], ['body', 'Body'], ['trainer', 'AI Trainer'], ['library', 'Library'], ['records', 'Records'], ['coach
+const navigation = [
+  ['dashboard', 'Command'],
+  ['workout', 'Workout'],
+  ['analytics', 'Analytics'],
+  ['body', 'Body'],
